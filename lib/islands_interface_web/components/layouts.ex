116c0d797d9e25f51f59057ccab8be85aad12fc3
Maciej Blim <maciej.blim@vistex.com>
2024-04-19 12:12:35 +0200

    generated new phx project

---
+++ lib/islands_interface_web/components/layouts.ex
@@ -0,0 +1,14 @@
+defmodule IslandsInterfaceWeb.Layouts do
+  @moduledoc """
+  This module holds different layouts used by your application.
+
+  See the `layouts` directory for all templates available.
+  The "root" layout is a skeleton rendered as part of the
+  application router. The "app" layout is set as the default
+  layout on both `use IslandsInterfaceWeb, :controller` and
+  `use IslandsInterfaceWeb, :live_view`.
+  """
+  use IslandsInterfaceWeb, :html
+
+  embed_templates "layouts/*"
+end
